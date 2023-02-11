eb9fc5d3ab9c464d41b34e6b35dd96a47f49e188
Emil Bertholdsson <carlemilamandus@gmail.com>
2023-02-11 10:32:51 +0100

    Trailer replaced

--- src/sections/Trailer.tsx
+++ src/sections/Trailer.tsx
@@ -7,7 +7,7 @@ const Trailer: React.FunctionComponent = (props: Props) => {
     return (
         <div className="pt-8 p-8 w-full flex flex-col">
             <h2 className="text-center">Trailer</h2>
-            <YoutubeVideo video_id="https://www.youtube.com/embed/FpoYp_MGL-8" />
+            <YoutubeVideo video_id="https://www.youtube.com/watch?v=cm31j5TudfE" />
         </div>
     );
 };
