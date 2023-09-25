4751e0a4d84449658a1f744fdf911494a75a90d6
Emil Bertholdsson <carlemilamandus@gmail.com>
2023-09-25 18:06:58 +0200

    Added steam link

--- src/App.tsx
+++ src/App.tsx
@@ -16,6 +16,17 @@ export function App() {
             <Hero />
             <div className="w-full place-items-center bg-holmgang-dark text-holmgang-beige">
                 <div className="md:container md:mx-auto mx-auto text-holmgang-beige content-div">
+                <div id="iframe-wrap">
+                    <iframe id="big-frame" className="m-auto" src="https://store.steampowered.com/widget/2403380/" width="646" height="190"></iframe>
+                    
+                    <div id="small-steamlink">
+                        <h3>ATGANGA: Spectacle for the Gods</h3>
+                        <p>
+                            Coming soon &nbsp;
+                            <a href="https://store.steampowered.com/app/2403380/ATGANGA_Spectacle_for_the_Gods/" target="_blank">Wishlist on Steam</a>
+                        </p>
+                    </div>
+                </div>
                     <Trailer />
                     <Game />
                     <Items />
